4c912b1a3f507ef62d40ca299a71750e330ca719
André Alexandre Gomes <andremegafone@gmail.com>
2019-08-03 20:14:12 +0100

    Update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+cd {{cookiecutter.package_name}}
+
 # install virtual environment
 conda env create -f dev_environment.yml
 source activate {{cookiecutter.package_name}}_dev_env
 
 # sphinx documentation
 cd doc
-sphinx-quickstart -q -a "{{cookiecutter.author_name}}" -p "{{ cookiecutter.package_name }}" -v "{{ cookiecutter.package_version}}"
-cd
+sphinx-quickstart -q -a "{{cookiecutter.author_name}}" -p "{{cookiecutter.package_name}}" -v "{{cookiecutter.package_version}}"
+cd ..
 
 # push to remote repo
 git init
