ba575a09e2fdbfdd662701735f9b977ffabd67d7
André Alexandre Gomes <andremegafone@gmail.com>
2019-08-03 19:16:04 +0100

    Update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# push to remote repo
-git init
-git add .
-git commit -m 'Initial commit'
-git push --set-upstream git@gitlab.com:{{cookiecutter.gitlab_account}}/{{cookiecutter.package_name}}.git master
-
 # install virtual environment
 conda env create -f dev_environment.yml
 source activate {{cookiecutter.package_name}}_dev_env
@@ -14,3 +8,9 @@ source activate {{cookiecutter.package_name}}_dev_env
 cd doc
 sphinx-quickstart -q -a "{{cookiecutter.author_name}}" -p "{{ cookiecutter.package_name }}" -v "{{ cookiecutter.package_version}}"
 cd
+
+# push to remote repo
+git init
+git add .
+git commit -m 'Initial commit'
+git push --set-upstream git@gitlab.com:{{cookiecutter.gitlab_account}}/{{cookiecutter.package_name}}.git master
